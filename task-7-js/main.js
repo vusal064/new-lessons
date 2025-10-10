4d158219606ea13e1cc50951f95ed63bdd3b08c7
vusal064 <vusal064@icloud.com>
2025-10-10 20:43:10 +0400

    add-js-7-new

--- task-7-js/main.js
+++ task-7-js/main.js
@@ -1,24 +1,19 @@
-let open = document.querySelector(".btn")
-let newDiv = document.querySelector(".container")
-let close = document.querySelector(".closeBtn")
+let open = document.querySelector(".btn");
+let newDiv = document.querySelector(".container");
+let close = document.querySelector(".closeBtn");
+let input = document.querySelector(".inpt");
+let btn2 = document.querySelector(".btn-2");
 
-
-newDiv.style.opacity = "0"
-
-
+newDiv.style.opacity = "0";
 
 open.addEventListener("click", () => {
-  newDiv.style.opacity = "1"
-  open.style.opacity = "0"
-  open.style.transition = " 0.4s "
-
-})
+  newDiv.style.opacity = "1";
+  open.style.opacity = "0";
+  open.style.transition = " 0.4s ";
+});
 
 close.addEventListener("click", () => {
-  newDiv.style.opacity = "0"
-  open.style.opacity = "1"
-    newDiv.style.transition = " 0.8s "
-
-})
-
-
+  newDiv.style.opacity = "0";
+  open.style.opacity = "1";
+  newDiv.style.transition = " 0.8s ";
+});
